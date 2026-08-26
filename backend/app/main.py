103967564d2ac0c8cf75396943d8c652e8370c38
kirusyag <xgalat@stuba.sk>
2026-08-26 21:48:35 +0200

    Add The FastAPI app itself and endpoints: User Calendar

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 #User(data that come from client)
-class UselCreate(BaseModel):
+class UserCreate(BaseModel):
     email: str
     nickname: str
     password: str
@@ -68,7 +68,8 @@ class NoteCreate(BaseModel):
 
 class Note(NoteCreate):
     id: int
-    
+
+#TEMPORARY STORAGE (instead of a database)
 user: dict = {}
 calendar: dict = {}
 lesson: dict = {}
@@ -89,35 +90,40 @@ def next_id(entity: str) -> int:
     return _counters[entity]
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-app = FastAPI()
+app = FastAPI(title="Study Planner API")
 @app.get("/")
 def root():
     return {"message": "Study Planner API"}
-@app.get("/calendars")
-def get_calendars():
-    calendars = [
-        {
-           "id": 1,
-            "name": "University",
-            "color": "#A8D8EA" 
-        },
-        {
-            "id": 2,
-            "name": "Personal",
-            "color": "#CDB4DB"
-        }
-    ]
-    return calendars
+#Users
+@app.post("/users", response_model = User)
+def create_user(data: UserCreate):
+    user_id = next_id("user")
+    user = {
+        "id": user_id,
+        "email": data.email,
+        "nickname": data.nickname,
+        "password": data.password, 
+    }  
+    users[user_id] = user
+    return user
+
+@app.get("/user/{user_id}", response_model = User)
+def get_user(user_id: int):
+    user = user.get(user_id)
+    if not user:
+        raise HTTPException(status_code = 404, details = "User not found")
+    return user
+
+#Calendar
+@app.post("/users/{user_id}/calendars/", response_model = Calendar)
+def create_calendar(user_id: int, data: CalendarCreate):
+    if user_id not in user:
+        raise HTTPException(status_code = 404, details = "User not found")
+    cal_id = next_id("calendar")
+    calendar = {"id": cal_id, "user_id": user_id, **data.model_dump()}
+    calendars[cal_id] = calendar
+    return calendar
+
+@app.get("/user/{user_id}/calendars/", response_model = list[Calendar])
+def list_calendar(user_id: int):
+    return [c for c in calendar.values() if c["user_id"] == user_id]
