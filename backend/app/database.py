fccc946838c2bdb177bf83d1e9231e086010b14b
kirusyag <xgalat@stuba.sk>
2026-09-09 22:29:16 +0200

    Update database configuration

--- backend/app/database.py
+++ backend/app/database.py
@@ -8,7 +8,7 @@ DATABASE_URL = "postgresql://mac@localhost:5432/study_planner"
 engine = create_engine(DATABASE_URL)
 
 #Session we will make queries to the database
-SessionLocal = sessionmaker(autocommit = False, autoflash = False, bind = engine)
+SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
 
 #Base - all our tables will inherit from it
 Base = declarative_base()
@@ -17,7 +17,7 @@ def get_db():
     """This function creates a "session" (connection) for a single 
     request and always closes it afterward, even if there was an 
     error. We'll use it in main.py via Depends(get_db)."""
-    bd = SessionLocal
+    db = SessionLocal()
     try:
         yield db
     finally: 
