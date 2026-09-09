2d6a68f5f1e8c1b543cfb1223d07e933f146712b
kirusyag <xgalat@stuba.sk>
2026-09-09 22:34:49 +0200

    Fix CORS configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -133,7 +133,7 @@ app.add_middleware(
     allow_origins = ["http://localhost:5173"],
     allow_credentials = True,
     allow_methods = ["*"],
-    allow_headers = ["*"]
+    allow_headers = ["*"],
 
 )
 
